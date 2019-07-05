f51e71f0abb67fc8a9a433d9e844fbec10d1eee2
darlansyah <darlansyah96@gmail.com>
2019-07-05 22:41:05 +0700

    menambahkan baris baru

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,3 +9,5 @@ header("location:srtdash/app/login/admin/login.php");
 //echo "darlan";
 // hello
 //C:\xampp\htdocs\perpustakaan\srtdash\app/login/admin/
+?>
+<h1> laode darlansyah <h1>
